0907db5cc023fb1c9539d3e37022fb51b0457fe8
Caio Santana <brnoliveirasantana@gmail.com>
2022-08-30 20:45:51 -0300

    projeto

---
+++ src/components/BarraNavegacao.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-bytebank">
+    <a class="navbar-brand" href="#">ByteBank</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <BarraNavegacaoLogin v-if="usuarioEstaLogado" />
+      <BarraNavegacaoLogout v-else />
+    </div>
+  </nav>
+</template>
+
+<script>
+import BarraNavegacaoLogin from "./BarraNavegacaoLogin.vue";
+import BarraNavegacaoLogout from "./BarraVavegacaoLogout.vue";
+
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    BarraNavegacaoLogin,
+    BarraNavegacaoLogout,
+  },
+
+  computed: {
+    ...mapGetters(["usuarioEstaLogado"]),
+  },
+};
+</script>
+
+<style>
+.navbar {
+  background: #27ae60;
+}
+.navbar-bytebank a {
+  color: #fff;
+}
+.navbar-bytebank a:hover {
+  color: #000;
+}
+</style>
---
+++ src/views/NovoUsuario.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <h1>Novo usuário</h1>
+    <form @submit.prevent="enviarFormulario">
+      <div class="form-group">
+        <label for="nome">Nome</label>
+        <input
+          id="nome"
+          type="text"
+          class="form-control"
+          v-model="usuario.nome"
+        />
+      </div>
+      <div class="form-group">
+        <label for="email">E-mail</label>
+        <input
+          id="email"
+          type="email"
+          class="form-control"
+          v-model="usuario.email"
+        />
+      </div>
+      <div class="form-group">
+        <label for="senha">Senha</label>
+        <input
+          id="senha"
+          type="password"
+          class="form-control"
+          v-model="usuario.senha"
+        />
+      </div>
+      <button class="btn btn-primary" type="submit">Salvar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      usuario: {
+        nome: "",
+        senha: "",
+        email: "",
+      },
+    };
+  },
+
+  methods: {
+    enviarFormulario() {
+      this.$http
+        .post("auth/register", this.usuario)
+        .then((resposta) => {
+          console.log(resposta);
+          this.$router.push({ name: "login" });
+        })
+        .catch((erro) => console.log(erro));
+    },
+  },
+};
+</script>
